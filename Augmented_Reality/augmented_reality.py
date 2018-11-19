0594d4b2d9feb3770072d128703b5217ebe1ff0b
zinsmatt <zins.matthieu@gmail.com>
2018-11-19 22:14:13 +0100

    Clean and reformat augmented reality

--- Augmented_Reality/augmented_reality.py
+++ Augmented_Reality/augmented_reality.py
@@ -8,6 +8,7 @@ import cv2
 import os
 import glob
 
+
 def axis_angle_to_matrix(v):
     """ Convert an axis-angle representation into a matrix_3x3
         representation of a rotation """
@@ -21,7 +22,7 @@ def axis_angle_to_matrix(v):
 
 
 # image = cv2.imread("data/images_undistorted/img_0001.jpg", cv2.IMREAD_COLOR)
-#image = cv2.imread("data/undist_images/img_0001.jpg", cv2.IMREAD_COLOR)
+# image = cv2.imread("data/undist_images/img_0001.jpg", cv2.IMREAD_COLOR)
 
 input_dir = "data/undist_images"
 output_dir = "data/AR"
@@ -66,24 +67,24 @@ pts_list = [pts, pts_2, pts_3]
 colors = [(0, 255, 0), (0, 0, 255), (255, 0, 0)]
 
 # 3D CAD model
-#points = []
-#faces = []
-#with open("data/teddy.obj", "r") as fin:
-#    lines = fin.readlines()
-#for l in lines:
-#    if l[:2] == "v ":
-#        points.append(list(map(float, l[2:].split(" "))))
-#    elif l[:2] == "f ":
-#        faces.append(list(map(int, l[2:].split(" "))))
-#points = np.vstack(points)
-#faces = np.vstack(faces) - 1
-#edges = []
-#for f in faces:
-#    edges.append([f[0], f[1]])
-#    edges.append([f[1], f[2]])
-#    edges.append([f[2], f[0]])
-#lines = edges
-#pts = points
+# points = []
+# faces = []
+# with open("data/teddy.obj", "r") as fin:
+#     lines = fin.readlines()
+# for l in lines:
+#     if l[:2] == "v ":
+#         points.append(list(map(float, l[2:].split(" "))))
+#      elif l[:2] == "f ":
+#         faces.append(list(map(int, l[2:].split(" "))))
+# points = np.vstack(points)
+# faces = np.vstack(faces) - 1
+# edges = []
+# for f in faces:
+#     edges.append([f[0], f[1]])
+#     edges.append([f[1], f[2]])
+#     edges.append([f[2], f[0]])
+# lines = edges
+# pts = points
 
 # Camera intrinsics
 K = np.loadtxt("data/K.txt")
@@ -106,20 +107,20 @@ for i, f in enumerate(sorted(list_of_images)):
         # 3D homogeneous points
         pts_h = np.c_[pts, np.ones((pts.shape[0], 1))]
         pts_h = pts_h.T
-    
+
         # to camera frame
         pts_c = Rt.dot(pts_h)
-    
+
         # projection at z=1
         pts_c_norm = pts_c / pts_c[2, :]
         # rescale by intrinsics
         uvs = K.dot(pts_c_norm)
-    
+
         # Draw points and lines
         uvs = uvs[:2, :].astype(int).T
         for uv in uvs:
             cv2.circle(image, tuple(uv), 2, colors[i], 1)
-    
+
         for edge in lines:
             cv2.line(image, tuple(uvs[edge[0]]), tuple(uvs[edge[1]]),
                      colors[i], 2, lineType=cv2.LINE_AA)
@@ -127,22 +128,21 @@ for i, f in enumerate(sorted(list_of_images)):
     cv2.imwrite(os.path.join(output_dir, name), image[:, :, ::-1])
     print(name, " done")
 
-## with distortion correction (not needed here)
-#xp = pts_c[0, :] / pts_c[2, :]
-#yp = pts_c[1, :] / pts_c[2, :]
+# #with distortion correction (not needed here)
+# xp = pts_c[0, :] / pts_c[2, :]
+# yp = pts_c[1, :] / pts_c[2, :]
 #
-#k1, k2 = np.loadtxt("data/D.txt")
-## k1, k2 = 0, 0
+# k1, k2 = np.loadtxt("data/D.txt")
 #
-#r2 = xp**2 + yp**2
+# r2 = xp**2 + yp**2
 #
-#factor = (1 + k1 * r2 + k2 * r2**2)
-#xpp = xp * factor
-#ypp = yp * factor
+# factor = (1 + k1 * r2 + k2 * r2**2)
+# xpp = xp * factor
+# ypp = yp * factor
 #
-#norm_pts = np.vstack((xpp, ypp, np.ones_like(xpp)))
-#uvs = K.dot(norm_pts)
+# norm_pts = np.vstack((xpp, ypp, np.ones_like(xpp)))
+# uvs = K.dot(norm_pts)
 
 # Display
-#cv2.namedWindow("fen")
-#cv2.imshow("fen", image)
+# cv2.namedWindow("fen")
+# cv2.imshow("fen", image)
