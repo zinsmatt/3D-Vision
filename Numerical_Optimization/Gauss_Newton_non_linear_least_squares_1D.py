9ca89819a3f79ba4e89d5cdb1d4cc8e57af6630e
Matthieu Zins <zins.matthieu@gmail.com>
2019-11-08 15:32:27 +0100

    Add examples for 1D case

---
+++ Numerical_Optimization/Gauss_Newton_non_linear_least_squares_1D.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Matthieu Zins
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+
+def f(x):
+    return 1-np.exp(-x)
+
+def grad_f(x):
+    return np.exp(-x)
+
+def grad2_f(x):
+    return -np.exp(-x)
+
+
+eps = 1e-12
+xmin, xmax = -1.5, 4.0
+
+# Residual and its first and second derivatives
+r = f
+grad_r = grad_f
+grad2_r = grad2_f
+
+X = np.linspace(xmin, xmax, 1000)
+Y = 0.5 * r(X)**2
+plt.plot(X, Y)
+
+x0 = 0.5
+
+# Here the total objective function that is minimized is called "cost"
+# and the 1d residual is r(). (In Ceres a costFunction corresponds to a 
+# residual block)
+cost = 0.5 * r(x0)**2
+prev_cost = cost + 1
+
+MAX_ITER = 200
+it = 0
+
+x = x0
+values = [x0]
+while abs(prev_cost - cost) > eps and it < MAX_ITER:
+    prev_cost = cost
+    
+    deriv = grad_r(x) * r(x)
+    deriv2 = grad_r(x) * grad_r(x) # + grad2_r(x) * r(x)
+    # in Gauss-Newton the second part of this  expression is neglected because 
+    # r(x) should be small (this avoids computing the second derivative grad2_r)
+    
+    
+    # second-order approximation
+    xx = np.linspace(x-0.5, x+0.5, 100)
+    approx_quadratic = 0.5*r(x)**2 + deriv * (xx - x) + 0.5 * deriv2 * (xx - x)**2
+    plt.plot(xx, approx_quadratic.flatten())
+    
+    # we are looking for the min of this approximation
+    #    f'(x + dx) = 0 = f'(x) + f''(x) * dx
+    # => dx = -f'(x) / f''(x)
+    #
+    # WARNING !!! it is important that f''(x) > 0
+    # otherwise we are going to find a maximum of the approximaiton
+    # (for N-dimensional case the Hessian matrix should be positive
+    # definite)
+    # => this is probably related to the condition that x0 should be 
+    # relatively "close" to the solution
+    #
+    
+    if deriv2 < 0:
+        print("WARNING: negative second derivative => diverge")
+        break
+    
+        
+    d = -deriv / deriv2
+    x = x + d
+    
+    cost = 0.5 * r(x)**2
+    it += 1
+    print("Iteration %d: %.4f x = %.4f" % (it, cost, x))
+    values.append(x)
+
+values = np.vstack(values)
+plt.plot(values[:,0], 0.5*r(values[:, 0])**2, marker='+', markersize=10, lineWidth=1, label="Newton")    
+
+print("Newton: Converged towards : %.2f in %d iterations" % (x, it))
+plt.legend()
--- Numerical_Optimization/Newton_non_linear_least_squares_1D.py
+++ Numerical_Optimization/Newton_non_linear_least_squares_1D.py
@@ -10,59 +10,34 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
 
+def f(x):
+    return 1-np.exp(-x)
 
-def f_convex1d(x):
-    return 2.0-np.exp(-(x - 1.0)**2)
-        
-def grad_f_convex1d(x):
-    return 2*(x-1)*np.exp(-(x-1)**2)
+def grad_f(x):
+    return np.exp(-x)
 
-def grad2_f_convex1d(x):
-    return 2*(-2*(x-1)*np.exp(-(x-1)**2) + np.exp(-(x-1)**2))
-
-
-def f_convex1d_2(x):
-    return (x-1)**2
-        
-def grad_f_convex1d_2(x):
-    return 2*(x-1)
-
-def grad2_f_convex1d_2(x):
-    return 2*x
-
-
-def f_nonconvex1d_1(x):
-    return x**4 - 14*x**3 + 60*x**2 - 70*x
-        
-def grad_f_nonconvex1d_1(x):
-    return 4*x**3 - 14*3*x**2 + 2*60*x - 70
-
-def grad2_f_nonconvex1d_1(x):
-    return 4*3*x**2 - 14*3*2*x + 2*60
-
-def f_test(x):
-    return (1-np.exp(-x))**2
-def grad_f_test(x):
-    return 2*(1-np.exp(-x))*2*np.exp(-x)
-def grad2_f_test(x):
-    return 2*np.exp(-2*x)-2*(1-np.exp(-x))*np.exp(-x)
+def grad2_f(x):
+    return -np.exp(-x)
 
 
 eps = 1e-12
-xmin, xmax = -0.5, 4.0
+xmin, xmax = -1.5, 4.0
 
-
-F = f_nonconvex1d_1
-grad_F = grad_f_nonconvex1d_1
-grad2_F = grad2_f_nonconvex1d_1
+# Residual and its first and second derivatives
+r = f
+grad_r = grad_f
+grad2_r = grad2_f
 
 X = np.linspace(xmin, xmax, 1000)
-Y = F(X)
+Y = 0.5 * r(X)**2
 plt.plot(X, Y)
 
-x0 = -0.33
+x0 = 0.5
 
-cost = F(x0)
+# Here the total objective function that is minimized is called "cost"
+# and the 1d residual is r(). (In Ceres a costFunction corresponds to a 
+# residual block)
+cost = 0.5 * r(x0)**2
 prev_cost = cost + 1
 
 MAX_ITER = 200
@@ -73,12 +48,15 @@ values = [x0]
 while abs(prev_cost - cost) > eps and it < MAX_ITER:
     prev_cost = cost
     
-    deriv = grad_F(x)
-    deriv2 = grad2_F(x)
+    deriv = grad_r(x) * r(x)
+    deriv2 = grad_r(x) * grad_r(x) + grad2_r(x) * r(x)
+    # in Gauss-Newton the second part of this  expression is neglected because 
+    # r(x) should be small (this avoids computing the second derivative grad2_r)
+    
     
     # second-order approximation (quadratic function)
     xx = np.linspace(x-0.5, x+0.5, 100)
-    approx_quadratic = F(x) + deriv * (xx - x) + 0.5 * deriv2 * (xx - x)**2
+    approx_quadratic = 0.5*r(x)**2 + deriv * (xx - x) + 0.5 * deriv2 * (xx - x)**2
     plt.plot(xx, approx_quadratic.flatten())
     
     # we are looking for the min of this approximation
@@ -89,6 +67,8 @@ while abs(prev_cost - cost) > eps and it < MAX_ITER:
     # otherwise we are going to find a maximum of the approximaiton
     # (for N-dimensional case the Hessian matrix should be positive
     # definite)
+    # => this is probably related to the condition that x0 should be 
+    # relatively "close" to the solution
     #
     
     if deriv2 < 0:
@@ -99,13 +79,13 @@ while abs(prev_cost - cost) > eps and it < MAX_ITER:
     d = -deriv / deriv2
     x = x + d
     
-    cost = F(x)
+    cost = 0.5 * r(x)**2
     it += 1
-    print("Iteration %d: %.4f" % (it, cost))
+    print("Iteration %d: %.4f x = %.4f" % (it, cost, x))
     values.append(x)
 
 values = np.vstack(values)
-plt.plot(values[:,0], F(values[:, 0]), marker='+', markersize=10, lineWidth=1, label="Newton")    
+plt.plot(values[:,0], 0.5*r(values[:, 0])**2, marker='+', markersize=10, lineWidth=1, label="Newton")    
 
 print("Newton: Converged towards : %.2f in %d iterations" % (x, it))
 plt.legend()
